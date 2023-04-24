494afb31f6c2fda0f4c2a698e99b9f24f21ecb16
UraniumNutt <ethummel5@gmail.com>
2023-04-24 16:29:32 -0500

    Started testing the ALU

--- CPU228/CPU228.srcs/sim_1/new/ALUTestbench.v
+++ CPU228/CPU228.srcs/sim_1/new/ALUTestbench.v
@@ -1,26 +1,56 @@
+// Ethan Thummel | Amir Bahmandar | Fabiola Sore 4/24/23
+
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 04/19/2023 11:01:42 AM
-// Design Name: 
-// Module Name: ALUTestbench
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
+module ALUTestbench;
 
-module ALUTestbench(
+    reg clk;
+    reg [15:0] A;
+    reg [15:0] B;
+    reg [4:0] functionSelect;
+    reg [4:0] overwriteFlagsMask;
+    reg [4:0] setFlagBits; // carry overflow zero pos neg
+    wire [15:0] result;
+    wire [4:0] currentFlags;    // carry overflow zero pos neg
+
+    ALU uut(
+
+        .clk(clk),
+        .A(A),
+        .B(B),
+        .functionSelect(functionSelect),
+        .overwriteFlagsMask(overwriteFlagsMask),
+        .setFlagBits(setFlagBits),
+        .result(result),
+        .currentFlags(currentFlags)
 
     );
+
+    initial begin
+
+        A = 14; B = 28; functionSelect = 1; 
+        #10 clk = 1; #10 clk = 0;
+
+        #10 functionSelect = 17; overwriteFlagsMask = 5'b11111; setFlagBits = 5'b00100;
+        #10 clk = 1; #10 clk = 0;
+
+        overwriteFlagsMask = 5'b11111; setFlagBits = 5'b00000;
+        #10 clk = 1; #10 clk = 0;
+
+        A = 42; B = 28; functionSelect = 3; 
+        #10 clk = 1; #10 clk = 0;
+
+        A = 7; B = 89; functionSelect = 5;
+        #10 clk = 1; #10 clk = 0;
+
+        overwriteFlagsMask = 5'b11111; setFlagBits = 5'b10000;
+        #10 clk = 1; #10 clk = 0;
+
+        A = 42; B = 50; functionSelect = 3; 
+        #10 clk = 1; #10 clk = 0;
+
+
+
+    end
+
 endmodule
--- CPU228/CPU228.srcs/sources_1/new/ALU.v
+++ CPU228/CPU228.srcs/sources_1/new/ALU.v
@@ -8,50 +8,122 @@ module ALU(
     input [15:0] A,
     input [15:0] B,
     input [4:0] functionSelect,
-    input [3:0] overwriteFlagsMask,
-    input updateFlags,
-    input [3:0] setFlagBits, // carry zero pos neg
+    input [4:0] overwriteFlagsMask,
+    input [4:0] setFlagBits, // carry overflow zero pos neg
     output [15:0] result,
-    output [3:0] FlagBits    // carry zero pos neg
+    output reg [4:0] currentFlags    // carry overflow zero pos neg
     
     );
 
-    reg [3:0] currentFlags;
-    initial currentFlags = 4'b0100;
+    // what the current state of the flags is, and how it should start
+    initial currentFlags = 5'b00100;
+    // a 17 bit result {carrybit, result}
+    reg [16:0] internalResult;
+    assign result = internalResult[15:0];
 
+    // instruction aliases
+    localparam ref   = 5'd0;  // ref  rx       - REFlect rx               | rx <-  rx
+    localparam add   = 5'd1;  // add  rx, *    - ADD with carry           | rx <-  rx + * + Cin
+    localparam adwc  = 5'd2;  // adwc rx, *    - ADd Without Carry        | rx <-  rx + *
+    localparam sub   = 5'd3;  // sub  rx, *    - SUBstract with carry     | rx <-  rx - * + Cin (?) ; Note: double check this
+    localparam suwb  = 5'd4;  // sbwc rx, *    - SuBstract Without Carry  | rx <-  rx - *
+    localparam mul   = 5'd5;  // mul  rx, *    - MULtiply                 | rx <-  rx * * ; Note: result truncated to 16 bits
+    localparam inc   = 5'd6;  // inc  rx       - INCrement                | rx <-  rx + 1
+    localparam dec   = 5'd7;  // dec  rx       - DECrement                | rx <-  rx - 1
+    localparam chs   = 5'd8;  // chs  rx       - CHange Sign              | rx <- ~rx + 1
+    localparam AND   = 5'd9;  // and  rx, *    - bitwise AND              | rx <-  rx & *
+    localparam OR    = 5'd10; // or   rx, *    - bitwise OR               | rx <-  rx | *
+    localparam NOT   = 5'd11; // not  rx       - bitwise NOT              | rx <- ~rx
+    localparam XOR   = 5'd12; // xor  rx, *    - bitwise XOR              | rx <-  rx xor *
+    localparam SL    = 5'd13; // sl   rx       - Shift Left               | rx <-  rx << 1
+    localparam SR    = 5'd14; // sr   rx       - Shift Right              | rx <-  rx >> 1
+    localparam cmp   = 5'd15; // cmp  rx, *    - CoMPare                  | rx - * ; Note: does not write back, only updates flags
+    localparam bit   = 5'd16; // bit  rx, *    - BIt Test                 | rx & * ; Note: does not write back, only updates flags
+    localparam changeFlags  = 5'd17; // do nothing to internalResult
 
     always @(functionSelect) begin
         
         case (functionSelect)
             
-            5'b00000: result = A;
-            5'b00001: result = A + B + currentFlags[3];  // add carry flag
-            5'b00010: result = A + B;
-            5'b00011: result = A - B - ~currentFlags[3]; // carry acts as a reverse borrow
-            5'b00100: result = A - B;
-            5'b00101: result = (A * B)[15:0];            // truncate
-            5'b00110: result = A + 1;
-            5'b00111: result = A - 1;
-            5'b01000: result = ~A + 1;
-            5'b01001: result = A & B;
-            5'b01010: result = A | B;
-            5'b01011: result = ~A;
-            5'b01100: result = A ^ B;
-            5'b01101: result = A << 1;
-            5'b01110: result = A >> 1;
-            5'b01111: result = A << B;
-            5'b10000: result = A >> B;
-            5'b10001: result = {(A << 1)[15:1], A[15]};   // to finish
-            5'b10010: result = {A[0], (A >> 1)[14:0]};    // to finish
-            5'b10011: result = 0;                         // to finish
-            5'b10100: result = 0;                         // to finish
-            5'b10101: result = A - B;                     // to finish, is this how this should work?
-            5'b10110: result = A * B;               
-
-
-            default: result = 0;
+            ref:   internalResult = A;
+            add:   internalResult = A + B + currentFlags[4];  // add carry flag
+            adwc:  internalResult = A + B;
+            sub:   internalResult = A - B + currentFlags[4];
+            suwb:  internalResult = A - B;
+            mul:   internalResult = (A * B);            // truncate
+            inc:   internalResult = A + 1;
+            dec:   internalResult = A - 1;
+            chs:   internalResult = ~A + 1;
+            AND:   internalResult = A & B;
+            OR:    internalResult = A | B;
+            NOT:   internalResult = ~A;
+            XOR:   internalResult = A ^ B;
+            SL:    internalResult = A << 1;
+            SR:    internalResult = A >> 1;  
+            cmp:   internalResult = A - B;                     
+            bit:   internalResult = A * B;               
 
         endcase
+
+    end
+
+    // updating the flags is synchronous
+    always @(posedge clk) begin
+
+        // explicitly change flags
+        if (functionSelect == changeFlags) begin
+            currentFlags = setFlagBits & overwriteFlagsMask;
+        end
+
+        else begin
+
+            // carry flag
+            if (internalResult[16] == 1) begin
+                currentFlags[4] = 1;
+            end
+
+            else begin
+                currentFlags[4] = 0;
+            end
+
+            // overflow flag
+            if ((internalResult[15] == 0 && A[15] == 1 && B[15] == 1) || (internalResult[15] == 1 && A[15] == 0 && B[15] == 0)) begin
+                currentFlags[3] = 1;
+
+            end
+
+            else begin
+                currentFlags[3] = 0;
+            end
+
+            //zero
+            if (internalResult[15:0] == 0) begin
+                currentFlags[2] = 1;
+            end
+
+            else begin
+                currentFlags[2] = 0;
+            end
+
+            // pos / neg flags
+
+            if (internalResult[15:0] > 0) begin
+                currentFlags[1] = 1;
+            end
+
+            else begin
+                currentFlags[1] = 0;
+            end
+
+            if (internalResult[15:0] < 0) begin
+                currentFlags[0] = 1;
+            end
+
+            else begin
+                currentFlags[0] = 0;
+            end
+
+        end
 
     end
 
