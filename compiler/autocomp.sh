d8ea1038f6558bdefa298dea3de5c771342b8ec2
uraniumnutt <ethummel5@gmail.com>
2023-05-07 14:28:04 -0500

    Demo branch

--- compiler/autocomp.sh
+++ compiler/autocomp.sh
@@ -1,4 +1,3 @@
-n1 = input.s
 customasm -f binstr $1 -o binstr.txt
 python3 linebreakconverter.py
 rm binstr.txt 
--- compiler/matrix.s
+++ compiler/matrix.s
@@ -2,8 +2,7 @@
 
 #include "CPU228.cpu"
 
-    ; matrix multiplication prototype
-    ; very rough prototype :)
+    ; 4 x 4 matrix multiplication
 
     nop
     ld r6, #0               ; init r7 with 0, use as row index (A's rows)
@@ -17,13 +16,13 @@ coll:                       ; loop for each collum
     jsr dotproduct          ; subroutine which calulates the dotproduct
 
     inc r7                  ; increment the collum index
-    cmp r7, #3              ; if its less that 3, then do the next collum loop
+    cmp r7, #4              ; if its less that 4, then do the next collum loop
     jnz coll
 
 collexit:                   ; when done with a collum             
 
     inc r6                  ; increment the row index
-    cmp r6, #3              ; if its less than 3, the do the nex row loop
+    cmp r6, #4              ; if its less than 4, the do the nex row loop
     jnz row
 
 rowexit:
@@ -37,7 +36,13 @@ rowexit:
     ld r0, C0+6
     ld r0, C0+7
     ld r0, C0+8
-
+    ld r0, C0+9
+    ld r0, C0+10
+    ld r0, C0+11
+    ld r0, C0+12
+    ld r0, C0+13
+    ld r0, C0+14
+    ld r0, C0+15
 
 programend:                 ; infinite loop at the end of the program
     jmp programend
@@ -61,55 +66,51 @@ dotproduct:
 
                             ; now do the math
     mul r3, r4              ; A X B
-    ld r0, r3               ; debug
     add r2, r3              ; increment running total by r3   
-    ld r0, r2               ; debug    
 
     inc r5                  ; increment the dotproduct index
-    cmp r5, #3              ; if the dotproduct index is less than 3, the do the next dotproduct loop
+    cmp r5, #4              ; if the dotproduct index is less than 3, the do the next dotproduct loop
     jnz .loop
 
 .exit:
 
     ld r3, CPointers[r6]    ; load r3 with the row pointer specified by the row index
-    ld r0, r3               ; debug
     add r3, r7              ; add the collum index to the pointer
-    ld r0, r3               ; debug
     st (r3), r2             ; store the dotproduct result for that cell into C
-    ld r0, r2               ; debug
     rts                     ; return from the dotproduct subroutine
 
-
-
-; The data structure will probably change for the final version
-; The final version should support N X N sizes
-
 APointers:
-#d16 A0, A1, A2 
+#d16 A0, A1, A2, A3 
 A0:
-#d16 1, 5, 9
+#d16 18, -14, -8, 5
 A1:
-#d16 6, 3, 2
+#d16 -6, 13, 18, 8
 A2:
-#d16 12, 3, 7
+#d16 -17, 20, -19, -19
+A3:
+#d16 -11, 12, 12, -3
 
 BPointers:
-#d16 B0, B1, B2
+#d16 B0, B1, B2, B3
 B0:
-#d16 5, 3, 5
+#d16 9, 0, 1, -8
 B1:
-#d16 9, 3, 2
+#d16 1, -2, -1, 6
 B2:
-#d16 2, 3, 4
+#d16 9, 9, -7, 0
+B3:
+#d16 -2, 5, -10, -3
 
 CPointers:
-#d16 C0, C1, C2
+#d16 C0, C1, C2, C3
 C0:
-#d16 0, 0, 0
+#d16 0, 0, 0, 0
 C1:
-#d16 0, 0, 0
+#d16 0, 0, 0, 0
 C2:
-#d16 0, 0, 0
+#d16 0, 0, 0, 0
+C3:
+#d16 0, 0, 0, 0
 
 #addr 0x0fff
 end:
